aad8a50b1237ac5a6da5970460f8e39ec8de04ad
NaseemAbdel <ben.wilgmann@hotmail.com>
2022-01-19 17:35:20 +0100

    Ported ProjektKS to .NET Framework 4.7.2

--- ProjektKS/ProjektKS/Program.cs
+++ ProjektKS/ProjektKS/Program.cs
@@ -14,7 +14,6 @@ namespace ProjektKS
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LogMeIn());
